914a43a958e5ec9285a248b961a832687091718a
waqasjavedkhan <solutiondesignandintegration@gmail.com>
2024-03-12 02:40:33 +0100

    Updated Midterm Certification Submission

--- Week 4 - Midterm Certification/app.py
+++ Week 4 - Midterm Certification/app.py
@@ -1,4 +1,4 @@
-# OpenAI Chat completion
+# importing required modules
 import os
 import getpass
 import wandb
@@ -56,28 +56,12 @@ retrieval_augmented_qa_chain = (
     | {"response": prompt | openai_chat_model, "context": itemgetter("context")}
 )
 
-# Importing the required modules, Creating a Retrieval Augmented QA Chain and initializing Weights & Biases project.
+# Creating a Retrieval Augmented QA Chain and initializing Weights & Biases project.
 retrieval_augmented_qa_chain = RetrievalQA.from_chain_type(
     llm=OpenAI(temperature=0.3),
     chain_type="stuff",
     retriever=retriever,
 )
-
-# Running evaluation and logging the results.
-#print(retrieval_augmented_qa_chain.run("Who is the E-VP, Operations - and how old are they?"))
-
-# Running evaluation and logging the results.
-#print(retrieval_augmented_qa_chain.run("What is the gross carrying amount of Total Amortizable Intangible Assets for Jan 29, 2023 in billions?"))
-
-# ChatOpenAI Templates
-#system_template = """You are a helpful assistant who always speaks in a pleasant tone!
-#"""
-
-#user_template = """{input}
-#Think through your response step by step.
-#"""
-
-
 @cl.on_chat_start  # marks a function that will be executed at the start of a user session
 async def start_chat():
     settings = {
@@ -91,7 +75,6 @@ async def start_chat():
 
     cl.user_session.set("settings", settings)
 
-
 @cl.on_message  # marks a function that should be run each time the chatbot receives a message from a user
 async def main(message: cl.Message):
     settings = cl.user_session.get("settings")
@@ -99,27 +82,8 @@ async def main(message: cl.Message):
     client = AsyncOpenAI()
 
     print(message.content)
-    
-#    prompt = Prompt(
-#        provider=ChatOpenAI.id,
-#        messages=[
-#            PromptMessage(
-#                role="system",
-#                template=system_template,
-#                formatted=system_template,
-#            ),
-#            PromptMessage(
-#                role="user",
-#                template=user_template,
-#                formatted=user_template.format(input=message.content),
-#            ),
-#        ],
-#        inputs={"input": message.content},
-#        settings=settings,
-#    )
-#
-#    print([m.to_openai() for m in prompt.messages])
 
+    # Initializing the logging.
     wandb.init(project="Midterm-Certification")
     
     query = message.content
@@ -133,19 +97,6 @@ async def main(message: cl.Message):
     
     msg = cl.Message(content="")
 
-#    # Call OpenAI
-#    async for stream_resp in await client.chat.completions.create(
-#        messages=[m.to_openai() for m in prompt.messages], stream=True, **settings
-#    ):
-#        token = stream_resp.choices[0].delta.content
-#        if not token:
-#            token = ""
-#        await msg.stream_token(token)
-#
-#    # Update the prompt object with the completion
-#    prompt.completion = msg.content
-#    msg.prompt = prompt
-    
     msg.content = response
 
     # Send and close the message stream
